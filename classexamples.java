d142165eb0f4dec67eb31469668304d22f35fd43
Nathan <youremail@>
2024-04-23 00:37:56 +0100

    first

---
+++ classexamples.java
@@ -0,0 +1,19 @@
+public class classexamples {
+
+    public static void main(String[] arg) {
+
+
+        boolean isEqual = 5==5;
+        boolean isNotEqual = 5==3;
+        boolean gte= 5>=2;
+        boolean lte= 5<=2;
+
+        System.out.println(isEqual);
+        System.out.println(isNotEqual);
+        System.out.println(gte);
+        System.out.println(lte);
+
+
+
+    }
+}
